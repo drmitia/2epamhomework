8e6f0432d24674a3d347504cb0f539b81d8bda96
drmitia <drdrmitia@gmail.com>
2024-03-18 22:13:43 +0200

    epam

---
+++ 2epamHomework(ex2)/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace _2epamHomework_ex2_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = Convert.ToInt32(Console.ReadLine());           
+            int position1 = number / 100;
+            int position2 = (number / 10) % 10;
+            int position3 = number % 10;
+                      
+            int max = Math.Max(position1, Math.Max(position2, position3));
+           
+            int min = Math.Min(position1, Math.Min(position2, position3));
+
+            int middle = position1 + position2 + position3 - max - min;
+
+            int result = max * 100 + middle * 10 + min;
+
+            Console.WriteLine($"Наше максимальне число: {result}");
+        }
+    }
+}
---
+++ 2epamhomework/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace _2epamhomework
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Скiльки коштує один зошит?");
+            int x = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Скiльки коштує одна ручка?");
+            int y = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Скiльки грошей має Петрик?");
+            int s = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Скiльки зошитiв хоче Петрик?");
+            int n = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Скiльки Ручок хоче Петрик?");
+            int m = Convert.ToInt32(Console.ReadLine());
+            if (x*n + y*m <= s) 
+            {
+            int b = s - x*n - y*m;
+            Console.WriteLine("У Петрика залишится " + b + " грн");
+            }
+            else 
+            {
+                Console.WriteLine("Петрик не може здiйнити таку покупку");
+            }
+        }
+    }
+}
